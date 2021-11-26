808018f1c21a5705017c04dda19742f9354d1db1
moensrc <94112988+moensrc@users.noreply.github.com>
2021-11-26 14:49:12 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
-// JavaScript Document
+// Get HTML elements and define as variables
+
+
+// More variables?
+
+
+// Open and close nav menu
+
+
+// Change fixed header based on scroll position 
+
+
+// Fixed positions for the header
