ffd41ddb1d0c1ccba63dbf057f39f298381893df
moensrc <94112988+moensrc@users.noreply.github.com>
2021-12-02 12:53:07 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,32 @@
 // Get HTML elements and define as variables
+var menuButton = document.querySelector("header section:first-of-type a");
+var navMenu = document.querySelector("header nav ul");
 
+var dopperLogo = document.querySelector("header section:first-of-type > img");
+var headerSection = document.querySelector("header section:first-of-type")
 
 // More variables?
 
 
-// Open and close nav menu
+menuButton.addEventListener("click", showMenu); 
 
+function showMenu() { // Open and close nav menu
+    navMenu.classList.toggle("showMenu")
+}
 
-// Change fixed header based on scroll position 
+// When the user scrolls down 10px from the top of the document, add class 'scrollHeader' and change logo
+window.onscroll = function() { 
+    scrollChangeMenu()
+}
+
+function scrollChangeMenu() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        headerSection.classList.toggle("scrollHeader");
+        dopperLogo.src = "./images/dopperblauw.svg";
+    } else {
+        console.log("what's happening?")
+    }
+  }
 
 
 // Fixed positions for the header
