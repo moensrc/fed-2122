16316b221d2b67c81bccfadd3efc38d5696cdf02
moensrc <94112988+moensrc@users.noreply.github.com>
2021-12-03 13:44:29 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,38 @@
 // Get HTML elements and define as variables
 var menuButton = document.querySelector("header section:first-of-type a");
+var menuIcon = document.querySelector("header section:first-of-type a img");
 var navMenu = document.querySelector("header nav ul");
 
 var dopperLogo = document.querySelector("header section:first-of-type > img");
-var headerSection = document.querySelector("header section:first-of-type")
+var headerSection = document.querySelector("header section:first-of-type");
 
 // More variables?
-
 
 menuButton.addEventListener("click", showMenu); 
 
 function showMenu() { // Open and close nav menu
-    navMenu.classList.toggle("showMenu")
+    navMenu.classList.toggle("showMenu");
+    menuIcon.classList.toggle("exitMenu");
+    menuIcon.src = "./images/cross.png"
+    dopperLogo.src = "./images/dopperblauw.svg";
 }
 
+// Fixed positions for the header
 // When the user scrolls down 10px from the top of the document, add class 'scrollHeader' and change logo
 window.onscroll = function() { 
     scrollChangeMenu()
 }
 
 function scrollChangeMenu() {
-    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-        headerSection.classList.toggle("scrollHeader");
-        dopperLogo.src = "./images/dopperblauw.svg";
+    if (document.body.scrollTop = 0){
+        return
     } else {
-        console.log("what's happening?")
+        headerSection.classList.add("scrollHeader");
+        dopperLogo.src = "./images/dopperblauw.svg";
     }
   }
 
 
-// Fixed positions for the header
+// Function for opening summary, change list type with class openSummary
+
+
